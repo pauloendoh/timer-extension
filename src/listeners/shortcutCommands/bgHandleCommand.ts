984e70a4ca6471928b0ca9fdd62c50e1a1ae5d3f
Paulo Endoh <paulo.endoh@gmail.com>
2023-03-21 23:20:58 -0300

    WHEN github discussion page, SHOULD show 'Discussion page not supported'. [clef0lunv000ox10ki3x31o30]

--- src/listeners/shortcutCommands/bgHandleCommand.ts
+++ src/listeners/shortcutCommands/bgHandleCommand.ts
@@ -1,9 +1,20 @@
+import { messageTypes } from '../../utils/messageTypes'
 import { bgHandleGithubIssuesCommand } from './bgHandleGithubIssuesCommand'
 
 export const bgHandleCommand = (command: string, tab: chrome.tabs.Tab) => {
   if (!tab.url) return
   if (command === 'nextIssue' || command === 'prevIssue') {
-    if (tab.url.includes('github.com') && tab.url.includes('/issues/')) {
+    if (
+      tab.url.includes('github.com') &&
+      (tab.url.includes('/issues/') || tab.url.includes('/discussions/'))
+    ) {
+      if (tab.url.includes('/discussions/')) {
+        chrome.tabs.sendMessage(tab.id!, {
+          type: messageTypes.alert,
+          message: 'Discussion page not supported',
+        })
+        return
+      }
       bgHandleGithubIssuesCommand(command, tab)
     }
   }
